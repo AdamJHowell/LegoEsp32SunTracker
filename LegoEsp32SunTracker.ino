b2cf7a2328277112785f6cf48f59af48fb0bc98b
AdamJHowell <adam.j.howell@gmail.com>
2022-10-07 10:40:00 -0600

    Updated config and readme files

--- LegoEsp32SunTracker.ino
+++ LegoEsp32SunTracker.ino
@@ -1,92 +1,73 @@
-/*
- * Servo specification:
- * Color: green
- * Torsion: 0.7kg
- * Operating voltage: 4.8-6V
- * Use Angle: 360 degrees
- * PWM signal: 500-2500
- * Wire length: 25CM
- * Terminal type: DuPont terminal
- * Gear reduction ratio: 1:140
- * Product speed: 160/ min (6V)
- */
-
 #include <ESP32Servo.h>
 
-Servo azimuthServo;  // create servo object to control a servo
-// 16 servo objects can be created on the ESP32
+Servo azimuthServo;
+int azimuthPosition = 90;
+int servoPin = 23;
 
-int pos = 90;    // variable to store the servo position
-// Recommended PWM GPIO pins on the ESP32 include 2,4,12-19,21-23,25-27,32-33
-// Possible PWM GPIO pins on the ESP32-S2: 0(used by on-board button),1-17,18(used by on-board LED),19-21,26,33-42
-#if defined(ARDUINO_ESP32S2_DEV)
-int servoPin = 23;
-#else
-int servoPin = 23;
-#endif
 
 void setup()
 {
 	Serial.begin( 115200 );
 	// Allow allocation of all timers
-	ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-	azimuthServo.setPeriodHertz(50);    // standard 50 hz servo
-	azimuthServo.attach(servoPin, 500, 2500); // attaches the servo on pin 23 to the servo object
-	azimuthServo.write( pos );
+	ESP32PWM::allocateTimer( 0 );
+	ESP32PWM::allocateTimer( 1 );
+	ESP32PWM::allocateTimer( 2 );
+	ESP32PWM::allocateTimer( 3 );
+	azimuthServo.setPeriodHertz( 50 );    // standard 50 hz servo
+	azimuthServo.attach( servoPin, 500, 2500 ); // attaches the servo on pin 23 to the servo object
+	azimuthServo.write( azimuthPosition );
 	// using default min/max of 1000us and 2000us
 	// different servos may require different min/max settings
 	// for an accurate 0 to 180 sweep
 	pinMode( 2, OUTPUT );
 }
 
+
 void loop()
 {
 	digitalWrite( 2, HIGH );
-	Serial.printf( "LED is %d\n", digitalRead( 2 ) );
+	Serial.printf( "LED is %d\n", digitalRead( 2 ));
 
 	// Ramp up from 90 to 180.
-	for( pos = 90; pos < 170; pos += 1 )
+	for( azimuthPosition = 90; azimuthPosition < 170; azimuthPosition += 1 )
 	{
-		azimuthServo.write( pos );
+		azimuthServo.write( azimuthPosition );
 		delay( 20 );
 	}
-	Serial.printf( "Servo at %d\n", pos );
+	Serial.printf( "Servo at %d\n", azimuthPosition );
 	delay( 3000 );
 	digitalWrite( 2, LOW );
-	Serial.printf( "LED is %d\n", digitalRead( 2 ) );
+	Serial.printf( "LED is %d\n", digitalRead( 2 ));
 
 	// Ramp down from 180 to 90.
-	for( pos = 170; pos > 90; pos -= 1 )
+	for( azimuthPosition = 170; azimuthPosition > 90; azimuthPosition -= 1 )
 	{
-		azimuthServo.write( pos );
+		azimuthServo.write( azimuthPosition );
 		delay( 20 );
 	}
-	Serial.printf( "Servo at %d\n", pos );
+	Serial.printf( "Servo at %d\n", azimuthPosition );
 	delay( 3000 );
 	digitalWrite( 2, HIGH );
-	Serial.printf( "LED is %d\n", digitalRead( 2 ) );
+	Serial.printf( "LED is %d\n", digitalRead( 2 ));
 
 	// Ramp down from 90 to 0.
-	for( pos = 90; pos > 10; pos -= 1 )
+	for( azimuthPosition = 90; azimuthPosition > 10; azimuthPosition -= 1 )
 	{
-		azimuthServo.write( pos );
+		azimuthServo.write( azimuthPosition );
 		delay( 20 );
 	}
-	Serial.printf( "Servo at %d\n", pos );
+	Serial.printf( "Servo at %d\n", azimuthPosition );
 	delay( 3000 );
 	digitalWrite( 2, LOW );
-	Serial.printf( "LED is %d\n", digitalRead( 2 ) );
+	Serial.printf( "LED is %d\n", digitalRead( 2 ));
 
 	// Ramp up from 0 to 90.
-	for( pos = 10; pos < 90; pos += 1 )
+	for( azimuthPosition = 10; azimuthPosition < 90; azimuthPosition += 1 )
 	{
-		azimuthServo.write( pos );
+		azimuthServo.write( azimuthPosition );
 		delay( 20 );
 	}
-	Serial.printf( "Servo at %d\n", pos );
+	Serial.printf( "Servo at %d\n", azimuthPosition );
 	delay( 3000 );
 
 	Serial.println( "End of loop.\n" );
--- README.md
+++ README.md
@@ -1,5 +1,60 @@
-# LegoEsp32SunTracker
+# Lego ESP32 Sun Tracker
 
 This is a simple sun tracker, made using Lego Technic pieces for the structural components, and using Lego-compatible servos to control azimuth and elevation.
 
-Servo Specifications
+## Green Servo Specifications:
+
+| Characteristic       | Value                          |
+|----------------------|--------------------------------|
+| Color:               | green                          |
+| Torsion:             | 0.7kg                          |
+| Operating voltage    | 4.8-6V                         |
+| Use Angle:           | 360 degrees, continuous        |
+| Limit angle:         | None                           |
+| Operating travel:    | 360° ±10° (in 500 → 2500 μsec) |
+| PWM signal:          | 500-2500                       |
+| Product speed        | 160/min @ 6V                   |
+| Gear reduction ratio | 1:140                          |
+| Wire length:         | 25 cm                          |
+| Terminal type:       | DuPont terminal                |
+
+## Gray Servo Specifications:
+
+### Electrical Specification (Function of the Performance)：
+
+| Characteristic               | 4.8V               | 6.0V               |
+|------------------------------|--------------------|--------------------|
+| Operating speed (at no load) | 0.14 ±0.01 sec/60° | 0.12 ±0.01 sec/60° |
+| Running current (at no load) | 70 ±20 mA          | 90 ±20 mA          |
+| Stall torque (at locked)     | 1.8 ±0.2 kg-cm     | 2 ±0.2 kg-cm       |
+| Stall current (at locked)    | 0.8 ±0.1 A         | 0.9 ±0.1 A         |
+| Idle current (at stopped)    | 7 ±1 mA            | 7 ±1 mA            |
+| Running life(at no load)     | > 50,000 times     | > 40,000 times     |
+| Temperature drift(at 25°C)   | ≤ 1°               | ≤ 1°               |
+
+### Mechanical Specification：
+
+| Characteristic          | value                                       |
+|-------------------------|---------------------------------------------|
+| Limit angle:            | 360°                                        |
+| Weight                  | 20 ±1g                                      |
+| Horn gear spline:       | Cross hole double output (1 Lego unit deep) |
+| Reduction ratio:        | 1/300                                       |
+| excessive play:         | ≤ 1°                                        |
+| Control system:         | Change the pulse width                      |
+| Amplifier type:         | Digital controller                          |
+| Operating travel:       | 360° ±10° (in 500 → 2500 μsec)              |
+| Left&Right travelling:  | ≤ 10°                                       |
+| Centering deviation:    | ≤ 2°                                        |
+| Neutral position:       | 1500 μsec                                   |
+| Dead bandwidth:         | ≤ 4 μsec                                    |
+| Rotating direction:     | counter-clockwise (1500 → 500 μsec)         |
+| Pulse width range:      | 500 → 2500 μsec                             |
+| Maximum travel          | 360°(500 → 2500 μsec)                       |
+| Shock resistance:       | 250 ±30g                                    |
+| Storage Temperature     | -20°C - 60°C                                |
+| Operating Temperature   | -10°C - 50°C                                |
+
+### Standard Test Environment
+
+Every test at temperature: 25 ±5° C and relative humidity: 65 ±10％.
